ce29ec3e20d0ef3789204334ecd7e8f4c0ab87cc
gul1010 <gulichka_rd@yahoo.com>
2021-03-16 19:25:12 -0700

    class14

---
+++ eclipse-workspace/JavaProgrammer/src/com/class14/Task.java
@@ -0,0 +1,17 @@
+package com.class14;
+
+public class Task {
+
+	public static void main(String[] args) {
+
+		
+		String str=new String(" Salom");
+		System.out.println(str);
+		System.out.println(str.length());
+		System.out.println(str.indexOf("S"));
+		System.out.println(str.charAt(4));
+        System.out.println(str.compareTo("Hello"));
+        System.out.println(str.trim());
+	}
+
+}
